16f6e78b0169079d984736062f294f6dc6bcc23c
Aashi Aggarwal <51317141+CRANBERR-Y@users.noreply.github.com>
2020-01-29 21:21:26 +0530

    Update mel_features.py

--- mel_features.py
+++ mel_features.py
@@ -18,14 +18,14 @@ with file:
     writer = csv.writer(file)
     writer.writerow(header)
 
-path = f"F:\ADocs\Year_3_Sem_2\Project\DCASE_UST\Dataset\Audio_dev"
+path = f"filepath" #filepath to the directory where train and validation folders are present
 dirs_1 = os.listdir(path)
 
 for filename in dirs_1:
-    path_1 = f"F:\ADocs\Year_3_Sem_2\Project\DCASE_UST\Dataset\Audio_dev\{filename}"
+    path_1 = path + "\{filename}"
     dirs_2 = os.listdir(path_1)
     for audiofile in dirs_2:
-        audio_snip = f'F:\ADocs\Year_3_Sem_2\Project\DCASE_UST\Dataset\Audio_dev\{filename}\{audiofile}'
+        audio_snip = path_1 + "\{audiofile}"
         y = librosa.load(audio_snip, sr = 44100)[0]
         mfccs = librosa.feature.mfcc(y=y, sr=44100, n_mels=128, fmin=20, fmax = 44100//2 )
         mfcc_delta = librosa.feature.delta(mfccs)[:,:18]
@@ -44,7 +44,7 @@ for filename in dirs_1:
             writer.writerow(to_append.split(','))
     
 feature_file = pd.read_csv('data.csv')
-excel_writer = pd.ExcelWriter('F:\ADocs\Year_3_Sem_2\Project\DCASE_UST\Codes\mfcc_dataset.xlsx', engine='xlsxwriter')
+excel_writer = pd.ExcelWriter('filepath\mfcc_dataset.xlsx', engine='xlsxwriter') #filepath  = where want to save
 feature_file.to_excel(excel_writer, 'mfcc_dataset')
 excel_writer.save()
 
